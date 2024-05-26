8c75a7c7d7f65985b7d69860a9924e899b2a5cff
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-05-26 14:05:23 +0400

    Update

--- backoffice/onboarding/customization/BShopOnboardingTour.vue
+++ backoffice/onboarding/customization/BShopOnboardingTour.vue
@@ -30,6 +30,7 @@
         rounded="xl"
         class="me-2"
         @click="select(item, i)"
+        v-track:click="`Onboarding ● Tour ● ${item.title}`"
       >
         <v-img :src="item.image" rounded="xl" class="ma-1" height="150"></v-img>
 
@@ -60,29 +61,39 @@
             :src="item.image"
             draggable
           >
-
           </v-carousel-item>
-
-
         </v-carousel>
 
         <v-sheet
-            style="position: absolute; bottom: 88px; left: 8px; right: 8px;backdrop-filter: blur(8px)"
-            class="pa-3 mx-auto typo-body"
-            rounded="sm"
-            color="#ffffffdd"
-            max-width="800"
-            border
+          style="
+            position: absolute;
+            bottom: 88px;
+            left: 8px;
+            right: 8px;
+            backdrop-filter: blur(8px);
+          "
+          class="pa-3 mx-auto typo-body"
+          rounded="sm"
+          color="#ffffffdd"
+          max-width="800"
+          border
         >
           <b class="d-block mb-2"> {{ items[index]?.title }}</b>
           {{ items[index]?.description }}
 
           <div v-if="items[index].action" class="py-2 text-end">
-            <v-btn  class="tnt" size="large" append-icon="open_in_new" :href="items[index].action.href" target="_blank">{{items[index].action.title}}</v-btn>
+            <v-btn
+              class="tnt"
+              size="large"
+              append-icon="open_in_new"
+              :href="items[index].action.href"
+              target="_blank"
+              v-track:click="`Onboarding ● Tour ● ${items[index].title} ● Action`"
+            >
+              {{ items[index].action.title }}
+            </v-btn>
           </div>
         </v-sheet>
-
-
       </v-card-text>
       <v-card-actions>
         <div class="widget-buttons">
@@ -118,7 +129,10 @@ export default {
         description:
           "With Avocado, you can offer services or products without predefined prices or details. Create a custom form for your customers to request services or products. Once they submit their request, you can set the price and process the payment. Share the form on your social media, allowing customers to add items, upload images, and submit orders easily.",
         image: require("./assets/avocado.jpg"),
-        action:{title:'Read About Avocado',href:'https://selldone.com/blog/avocado-social-sell-tool-116'}
+        action: {
+          title: "Read About Avocado",
+          href: "https://selldone.com/blog/avocado-social-sell-tool-116",
+        },
       },
 
       {
@@ -126,8 +140,10 @@ export default {
         description:
           "Build a thriving marketplace with Selldone by adding multiple vendors. Vendors can list their products, and you can set a commission rate for each sale. When a customer makes a purchase, the vendor receives the payment directly, manages the order through their dedicated panel, and you earn a commission via the auto payout feature.",
         image: require("./assets/marketplace.jpg"),
-        action:{title:'Auto Vendors Payout',href:'https://selldone.com/blog/stripe-connect-payout-marketplace-120'}
-
+        action: {
+          title: "Auto Vendors Payout",
+          href: "https://selldone.com/blog/stripe-connect-payout-marketplace-120",
+        },
       },
       {
         title: "Support Complex Business Models",
@@ -138,7 +154,7 @@ export default {
       {
         title: "Product Locations Restriction & Gift Shopping",
         description:
-            "Set location restrictions for each product by specifying multiple countries, ZIP codes, and cities. This feature is perfect for creating a personalized gift shopping experience or limiting product availability to specific locations, such as for restaurants.",
+          "Set location restrictions for each product by specifying multiple countries, ZIP codes, and cities. This feature is perfect for creating a personalized gift shopping experience or limiting product availability to specific locations, such as for restaurants.",
         image: require("./assets/product-location.jpg"),
       },
     ],
