595fae0751f41f7d242b839e7c8f95e8d257987e
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2024-09-01 12:59:38 +0400

    Update

--- ui/widget/help/SWidgetHelp.vue
+++ ui/widget/help/SWidgetHelp.vue
@@ -13,67 +13,70 @@
   -->
 
 <template>
-  <v-btn
-    @click="dialog = true"
-    class="tnt"
-    variant="text"
-    color="#4f46e5"
-    slim
-    prepend-icon="tips_and_updates"
-  >
-    <span style="text-decoration: underline">
-      {{ title }}
-    </span>
-  </v-btn>
-  <v-dialog
-    v-model="dialog"
-    fullscreen
-    scrollable
-    transition="dialog-bottom-transition"
-  >
-    <v-card @click="dialog = false" color="#fff" class="text-start">
-      <v-card-title class="d-flex align-center">
+  <template v-if="embed_code">
+    <v-btn
+      @click="dialog = true"
+      class="tnt"
+      variant="text"
+      color="#4f46e5"
+      slim
+      prepend-icon="tips_and_updates"
+    >
+      <span style="text-decoration: underline">
         {{ title }}
-        <v-spacer></v-spacer>
-        <v-btn variant="text" icon @click="dialog = false" class="ms-2">
-          <v-icon>close</v-icon>
-        </v-btn>
-      </v-card-title>
-      <v-card-text>
-        <div
-          v-if="code"
-          style="
-            position: relative;
-            box-sizing: content-box;
-            max-height: 80vh;
-            max-height: 80svh;
-            width: 100%;
-            aspect-ratio: 1.4588963963963963;
-            max-width: 1200px;
-            margin: auto;
-          "
-        >
-          <iframe
-            :src="`https://demo.selldone.com/embed/${code}?embed_v=2`"
-            loading="lazy"
-            title="Cross Selling"
-            allow="clipboard-write"
-            frameborder="0"
-            webkitallowfullscreen="true"
-            mozallowfullscreen="true"
-            allowfullscreen
+      </span>
+    </v-btn>
+    <v-dialog
+      v-model="dialog"
+      fullscreen
+      scrollable
+      transition="dialog-bottom-transition"
+    >
+      <v-card @click="dialog = false" color="#fff" class="text-start">
+        <v-card-title class="d-flex align-center font-weight-thin">
+          <v-icon class="me-2" size="16">tips_and_updates</v-icon>
+          {{ title }}
+          <v-spacer></v-spacer>
+          <v-btn variant="text" icon @click="dialog = false" class="ms-2">
+            <v-icon>close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <div
+            v-if="embed_code"
             style="
-              position: absolute;
-              top: 0;
-              left: 0;
+              position: relative;
+              box-sizing: content-box;
+              max-height: 80vh;
+              max-height: 80svh;
               width: 100%;
-              height: 100%;
+              aspect-ratio: 1.4588963963963963;
+              max-width: 1200px;
+              margin: auto;
             "
-          ></iframe>
-        </div>
-      </v-card-text>
-    </v-card>
-  </v-dialog>
+          >
+            <iframe
+              :src="`https://demo.selldone.com/embed/${embed_code}?embed_v=2`"
+              loading="lazy"
+              title="Cross Selling"
+              allow="clipboard-write"
+              frameborder="0"
+              webkitallowfullscreen="true"
+              mozallowfullscreen="true"
+              allowfullscreen
+              style="
+                position: absolute;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+              "
+            ></iframe>
+          </div>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+  </template>
 </template>
 
 <script lang="ts">
@@ -85,13 +88,18 @@ export default defineComponent({
     code: {
       required: true,
     },
-    title: {
-      required: true,
-    },
   },
   data: () => ({
     dialog: false,
   }),
+  computed: {
+    embed_code() {
+      return this.$tm("help." + this.code)?.embed;
+    },
+    title() {
+      return this.$tm("help." + this.code)?.title;
+    },
+  },
 });
 </script>
 
