28e5ddaf223b730104d7749205475435fccd792c
Mehrdad Pajuhaan <pajuhaan@gmail.com>
2026-05-20 22:03:43 +0400

    Update

--- article/add-on/catalog/flipbook/matrix.ts
+++ article/add-on/catalog/flipbook/matrix.ts
@@ -12,50 +12,109 @@
  * Tread carefully, for you're treading on dreams.
  */
 
-import {
-    identity,
-    multiply,
-    perspective,
-    translate,
-    translate3d,
-    rotateY,
-    toString
-} from "rematrix";
+type Matrix4 = [
+  number, number, number, number,
+  number, number, number, number,
+  number, number, number, number,
+  number, number, number, number,
+];
+
+type MatrixInput = Matrix | Matrix4 | number[];
+
+function identity(): Matrix4 {
+  return [
+    1, 0, 0, 0,
+    0, 1, 0, 0,
+    0, 0, 1, 0,
+    0, 0, 0, 1,
+  ];
+}
+
+function multiply(a: number[], b: number[]): Matrix4 {
+  const out = new Array<number>(16).fill(0);
+
+  for (let row = 0; row < 4; row++) {
+    for (let col = 0; col < 4; col++) {
+      for (let i = 0; i < 4; i++) {
+        out[col * 4 + row] += a[i * 4 + row] * b[col * 4 + i];
+      }
+    }
+  }
+
+  return out as Matrix4;
+}
+
+function perspective(distance: number): Matrix4 {
+  const out = identity();
+  out[11] = -1 / distance;
+  return out;
+}
+
+function translate(x: number, y: number): Matrix4 {
+  return translate3d(x, y, 0);
+}
+
+function translate3d(x: number, y: number, z: number): Matrix4 {
+  const out = identity();
+  out[12] = x;
+  out[13] = y;
+  out[14] = z;
+  return out;
+}
+
+function rotateY(deg: number): Matrix4 {
+  const rad = (deg * Math.PI) / 180;
+  const cos = Math.cos(rad);
+  const sin = Math.sin(rad);
+
+  return [
+    cos, 0, -sin, 0,
+    0, 1, 0, 0,
+    sin, 0, cos, 0,
+    0, 0, 0, 1,
+  ];
+}
+
+function toString(matrix: number[]): string {
+  return `matrix3d(${matrix.join(",")})`;
+}
 
 export default class Matrix {
-    constructor(arg) {
-        if (arg) {
-            if (arg.m) {
-                this.m = [...arg.m];
-            } else {
-                this.m = [...arg];
-            }
-        } else {
-            this.m = identity();
-        }
+  m: Matrix4;
+
+  constructor(arg?: MatrixInput) {
+    if (arg) {
+      if (arg instanceof Matrix) {
+        this.m = [...arg.m];
+      } else {
+        this.m = [...arg] as Matrix4;
+      }
+    } else {
+      this.m = identity();
     }
-    clone() {
-        return new Matrix(this);
-    }
-    multiply(m) {
-        this.m = multiply(this.m, m);
-    }
-    perspective(d) {
-        this.multiply(perspective(d));
-    }
-    transformX(x) {
-        return (x * this.m[0] + this.m[12]) / (x * this.m[3] + this.m[15]);
-    }
-    translate(x, y) {
-        this.multiply(translate(x, y));
-    }
-    translate3d(x, y, z) {
-        this.multiply(translate3d(x, y, z));
-    }
-    rotateY(deg) {
-        this.multiply(rotateY(deg));
-    }
-    toString() {
-        return toString(this.m);
-    }
+  }
+  clone() {
+    return new Matrix(this);
+  }
+  multiply(m: number[]) {
+    this.m = multiply(this.m, m);
+  }
+  perspective(d: number) {
+    this.multiply(perspective(d));
+  }
+  transformX(x: number) {
+    return (x * this.m[0] + this.m[12]) / (x * this.m[3] + this.m[15]);
+  }
+  translate(x: number, y: number) {
+    this.multiply(translate(x, y));
+  }
+  translate3d(x: number, y: number, z: number) {
+    this.multiply(translate3d(x, y, z));
+  }
+  rotateY(deg: number) {
+    this.multiply(rotateY(deg));
+  }
+  toString() {
+    return toString(this.m);
+  }
 }
